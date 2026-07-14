eac0a8d421832a6e9efbbabf1c4b8fff4554e442
goormany <zarbot351@yandex.ru>
2026-07-14 21:44:48 +0300

    fix: rename timeslots datamapper

--- src/data_mappers/time_slots.py
+++ src/data_mappers/time_slots.py
@@ -2,6 +2,6 @@ from src.data_mappers.base import BaseDataMapper
 from src.models.time_slots import TimeSlots
 from src.schemas.time_slots import TimeSlotsResponse
 
-class RoomDataMapper(BaseDataMapper):
+class TimeSlotsDataMapper(BaseDataMapper):
     db_model = TimeSlots
     schema = TimeSlotsResponse
