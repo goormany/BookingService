9208a5d1862bddf1461182927692958a7756305a
goormany <zarbot351@yandex.ru>
2026-07-14 21:42:26 +0300

    fix: rename users datamapper

--- src/data_mappers/users.py
+++ src/data_mappers/users.py
@@ -2,6 +2,6 @@ from src.data_mappers.base import BaseDataMapper
 from src.models.users import Users
 from src.schemas.users import UserResponse
 
-class RoomDataMapper(BaseDataMapper):
+class UserDataMapper(BaseDataMapper):
     db_model = Users
     schema = UserResponse
