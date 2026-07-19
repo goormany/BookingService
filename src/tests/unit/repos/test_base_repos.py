bafdee2094b9bfbcb60f175246a19426a307135a
goormany <zarbot351@yandex.ru>
2026-07-19 14:14:15 +0300

    add test raises base repos not found in delete func

--- src/tests/unit/repos/test_base_repos.py
+++ src/tests/unit/repos/test_base_repos.py
@@ -289,3 +289,13 @@ class TestDelete:
         assert isinstance(result, str)
         assert result == "map_test_model_1"
         mock_session.execute.assert_called_once()
+
+    async def test_delete_raise_not_found(self, repo, mock_session, args, kwargs):
+        mock_result = MagicMock()
+        mock_result.scalar_one.side_effect = NoResultFound
+        mock_session.execute.return_value = mock_result
+        
+        with pytest.raises(BookingRoomsNotFoundObjException):
+            await repo.delete(*args, **kwargs)
+        
+        mock_session.execute.assert_called_once()
