5d72e60fa9a9cf4fbd80c886d0f7c69477a12435
goormany <zarbot351@yandex.ru>
2026-09-10 12:15:15 +0300

    ruff format code

--- src/middleware/disable_cache_control.py
+++ src/middleware/disable_cache_control.py
@@ -4,10 +4,11 @@ from fastapi import Request, Response
 
 from src.main import app
 
+
 @app.middleware("http")
 async def cache_control_reset(request: Request, call_next: Callable):
     response: Response = await call_next(request)
-    
+
     response.headers["Cache-Control"] = "no-cache"
-    
+
     return response
